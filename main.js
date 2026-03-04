2cd0d892aef373755e4955cfda8eb6cd62e80190
cdnvideycom <videystcom@gmail.com>
2026-03-04 09:38:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function joinWhatsAppGroup() {
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/share/1SDPL52rAX/");
+    window.open("https://www.facebook.com/share/1FXwSoMVgM/");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
@@ -34,6 +34,7 @@ overlay.addEventListener("click", () => {
     // Aksi setelah klik
     window.open("https://s.shopee.co.id/5fjkFdBJdC", "_blank");
 });
+
 
 
 
